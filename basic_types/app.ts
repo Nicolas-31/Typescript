0fcaa028792323d70d5b2c962e67391a6c08823f
Nicolas Anderson <nicolas-31@outlook.com>
2023-01-07 20:41:12 +0100

    updated

--- basic_types/app.ts
+++ basic_types/app.ts
@@ -8,3 +8,8 @@ if(typeof userInput === 'string') {
 }
 
 
+function  generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+generateError('An error occurred!', 500);
